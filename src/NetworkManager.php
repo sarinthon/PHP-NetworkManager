34a125efff829c4925be282ab14d4a507689d8f6
Sarinthon Mangkorn-ngam <sarinthonton@gmail.com>
2022-07-22 20:49:50 +0700

    Initial commit

---
+++ src/NetworkManager.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace ShuGlobal\NetworkManager;
+
+use GuzzleHttp\Client;
+
+enum NetworkMethod: string {
+    case GET = "GET";
+    case POST = "POST";
+}
+
+class NetworkManager
+{
+    public static function request($url, $body, $headers=null, NetworkMethod $method=NetworkMethod::POST): ?object {
+        $client = new Client();
+        $response = $client->request($method->value, $url, [
+            'body'=> $body,
+            'headers' => $headers ?? [
+                    'Accept' => 'text/plain',
+                    'Content-Type' => 'application/json'
+                ],
+        ]);
+        // Response
+        $responseBody = $response->getBody();
+
+        if (($headers['Accept'] ?? "") == "application/jose") {
+            return str($responseBody->getContents());
+        }
+
+        return json_decode($responseBody, false);
+    }
+}
